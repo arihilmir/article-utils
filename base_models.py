cf2e248a16f08a37f3cbae4d7a37e1475b21c9aa
Arihilmir <me@arihilmir.com>
2024-12-13 11:55:56 +0200

    Add stacking model

--- base_models.py
+++ base_models.py
@@ -12,6 +12,40 @@ def lstm_block(hidden_size, layers, out_size=24, input_shape=(72, 4)):
     model.add(l.Dense(out_size, activation='silu'))
     return model
 
+def get_big_model(num_units, num_layers):
+    model = keras.Sequential()
+    model.add(l.Input((72, 4), batch_size=4))
+
+    for _ in range(num_layers):
+        model.add(l.GRU(num_units, return_sequences=True))
+
+    model.add(l.GRU(num_units))
+    model.add(l.Dense(64, activation='silu'))
+    model.add(l.Dense(24))
+
+    model.compile(loss=keras.losses.MeanSquaredError(),
+                  optimizer=keras.optimizers.Adam(learning_rate=0.0005),
+                  metrics=[keras.metrics.MeanAbsolutePercentageError(),
+                           keras.metrics.RootMeanSquaredError(),
+                           keras.metrics.R2Score()])
+
+    return model
+
+def separated_model(hidden_size, layers, out_size=24, input_shape=(72, 4), bsz=4):
+    inputs = l.Input(input_shape, bsz)
+    pc, w = ops.split(inputs, [1], axis=-1)
+    for _ in range(layers-1):
+        pc = l.GRU(hidden_size, return_sequences=True)(pc)
+        w = l.GRU(hidden_size, return_sequences=True)(w)
+    pc = l.GRU(hidden_size)(pc)
+    w = l.GRU(hidden_size)(w)
+    xs = ops.stack([pc, w], axis=-1)
+    out =  l.GRU(2*hidden_size, name='stacking_gru')(xs)
+    out = l.Dense(128, activation='silu', name='stacking_dense_1')(out)
+    out = l.Dense(out_size, name='stacking_dense_2')(out)
+    model = keras.Model(inputs=inputs, outputs=out, name="Stacking")
+    return model
+
 def stacking_model(models_paths: list[str], input_shape = (72,4), bsz=4):
     inputs = l.Input(input_shape, batch_size=bsz)
     models = [keras.saving.load_model(path) for path in models_paths]
